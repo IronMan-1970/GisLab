8c90fed9a835e7928184dce2614ec04bb1a56c8e
umaks <antihemandb@gmail,com>
2025-05-14 10:20:33 +0300

    LAB1

--- Lab_1/task_3.py
+++ Lab_1/task_3.py
@@ -4,10 +4,10 @@ import math
 def get_centroid_coords(feature):
     geometry = feature.GetGeometryRef()
     centroid = geometry.Centroid()
-    return centroid.GetY(), centroid.GetX()  # Повертаємо як (lat, lon)
+    return centroid.GetY(), centroid.GetX()
 
 def haversine(lat1, lon1, lat2, lon2):
-    R = 6371  # Радіус Землі в км
+    R = 6371
     rLat1, rLon1 = math.radians(lat1), math.radians(lon1)
     rLat2, rLon2 = math.radians(lat2), math.radians(lon2)
     dLat = rLat2 - rLat1
@@ -16,15 +16,12 @@ def haversine(lat1, lon1, lat2, lon2):
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     return R * c
 
-# Відкриваємо файл
 shapefile = osgeo.ogr.Open("ukraine_Village_Councils_level_3.shp")
 layer = shapefile.GetLayer(0)
 
-# Вибір двох об'єктів (наприклад, ID 5 і 9 — заміни на потрібні)
 feature1 = layer.GetFeature(5)
 feature2 = layer.GetFeature(9)
 
-# Отримуємо центроїди
 lat1, lon1 = get_centroid_coords(feature1)
 lat2, lon2 = get_centroid_coords(feature2)
 
